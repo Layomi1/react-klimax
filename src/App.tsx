4d310c21e49dcac35c620f3a55c07f99d30ef588
Layomi <mac@MACs-MacBook-Pro.local>
2025-07-24 19:14:51 +0100

    setting up project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout>Hello</Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
